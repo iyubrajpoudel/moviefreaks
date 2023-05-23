cad105454931d051ff88858dc925bae556ef2879
Yubraj <iyubrajpoudel@gmail.com>
2023-05-23 11:40:08 +0545

    Header completed...

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const App = () => {
   return (
     <>
     <BrowserRouter>
-      {/* <Header/> */}
+      <Header/>
         <Routes>
           <Route path={`/`} element={<Home/>} />
           <Route path={`/:mediaType/:id`} element={<Details/>} />
